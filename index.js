e3bfdc5b213b6a8f676b8315d2cbd88946efd95f
GuHong Min <placidmoon1@gmail.com>
2021-02-05 11:40:31 +0900

    title page done

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-Vue.component('todo-item', {
-  template: '<li>This is a todo</li>'
-})
-
-var app = new Vue({
-  el: '#app'
-})
-
-var app2 = new Vue({
-  el: '#app-2',
-  data: {
-    message: 'You loaded this page on ' + new Date().toLocaleString()
-  }
-})
+window.onload = function(){
+  $('#enter-magic-code').on('input', function(){
+    let text = document.getElementById('enter-magic-code').value;
+    let login_button =  document.getElementById("login-button");
+    console.log(text);
+    if (text == '') {
+      console.log('empty');
+      if (login_button.classList.contains('active-button'))
+        login_button.classList.remove("active-button");
+      login_button.classList.add('passive-button');
+      login_button.disabled = true;
+    } else {
+      if (login_button.classList.contains('passive-button'))
+        login_button.classList.remove("passive-button");
+      login_button.classList.add('active-button');
+      login_button.disabled = false;
+    }
+  });
+}
